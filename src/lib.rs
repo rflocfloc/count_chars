00a08b78c6f45611172b17949b35468a589cd942
rflocfloc <riccardoscandino@gmail.com>
2024-02-24 19:10:22 +0100

    counting function and loading data

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,50 @@
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
 
-pub fn count_specials(line:&str, char_hm:&mut HashMap<String, i32>) {
 
-    for chr in line.chars(){
-        if !chr.is_alphanumeric() && chr != ' '{
-           char_hm.entry(chr.to_string()).and_modify(|x| *x += 1).or_insert(1); 
+pub fn get_line_counts(line:&str)-> HashMap<char, u32>{
+    let counts:HashMap<char,u32> = line.chars()
+        .filter(|x| x != &' ')
+        .fold(HashMap::new(), |mut acc, c| {
+            acc.entry(c.to_ascii_lowercase()).and_modify(|x| *x +=1).or_insert(1u32);
+
+            acc
+        });
+    return counts
+}
+
+
+
+fn read_file_line_by_line(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        match line {
+            // line is a String
+            Ok(line) => process_line(line),
+            Err(err) => handle_error(err),
         }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
-mod check_characters{
+mod libtest{
     use super::*;
 
     #[test]
-    fn count_specials_test(){
-        let line:&str = "Hello! my name is FlocFlocFloc!1! And this is a f*|*|g test 100%";
-        let mut result:HashMap<String, i32> = HashMap::new(); 
-        count_specials(line, &mut result);
-        assert_eq!(result, HashMap::from([("!".to_string(), 3),("*".to_string(), 2), ("|".to_string(),2),("%".to_string(), 1)]));
-    }
+    fn get_line_counts_test(){
+        let line:&str = "aaa !! 11111 B";
+        let test:HashMap<char,u32> = get_line_counts(line);
+        let solution:HashMap<char,u32> = HashMap::from([
+            ('a', 3),
+            ('!', 2),
+            ('1', 5),
+            ('b', 1),
+        ]);
+        assert_eq!(test, solution);
+   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
+
 mod lib;
 use lib::*;
 
 
 fn main() {
-    let line = "Hello, world!";
-    let mut result:HashMap<String, i32> = HashMap::new(); 
-    count_specials(line, &mut result);
-    println!("{:?}", result);
+    let line = "Hello! my name is FlocFlocFloc!1! And this is a f*|*|g test 100%";
+    let test:HashMap<char,u32> = get_line_counts(line);
+    let path = "./test.txt";
+    let test:HashMap<char,u32> = read_lines_and_counts(path);
+    println!("{:?}", test);
 }
